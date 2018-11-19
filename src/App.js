9623d6b09c4c79521bebc8930feb65fe9d354ef4
Panu Oksiala <panu@oksiala.fi>
2018-11-19 02:28:49 +0200

    part 2-06

--- src/App.js
+++ src/App.js
@@ -1,52 +1,64 @@
-import React from 'react'
-import Kurssi from './components/kurssi'
+import React from 'react';
 
-const App = () => {
-  const kurssit = [
-    {
-      nimi: 'Half Stack -sovelluskehitys',
-      id: 1,
-      osat: [
-        {
-          nimi: 'Reactin perusteet',
-          tehtavia: 10,
-          id: 1
-        },
-        {
-          nimi: 'Tiedonvälitys propseilla',
-          tehtavia: 7,
-          id: 2
-        },
-        {
-          nimi: 'Komponenttien tila',
-          tehtavia: 14,
-          id: 3
-        }
-      ]
-    },
-    {
-      nimi: 'Node.js',
-      id: 2,
-      osat: [
-        {
-          nimi: 'Routing',
-          tehtavia: 3,
-          id: 1
-        },
-        {
-          nimi: 'Middlewaret',
-          tehtavia: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+const Person = ({person}) => <li>{person.name}</li>
+
+const Persons = ({persons}) => {
+  const personList = persons.map(person =>
+    <Person key={person.name} person={person} />)
 
   return (
-    <div>
-      {kurssit.map(kurssi => <Kurssi key={kurssi.id} kurssi={kurssi} />)}
-    </div>
+    <ul>
+      {personList}
+    </ul>
   )
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      persons: [
+        { name: 'Arto Hellas' }
+      ],
+      newName: ''
+    }
+  }
+  
+  addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: this.state.newName
+    }
+
+    const persons = this.state.persons.concat(personObject)
+
+    this.setState({persons, newName: ''})
+  }
+
+  handleNameChange = (event) => {
+    event.preventDefault()
+    this.setState({newName: event.target.value})
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Puhelinluettelo</h2>
+        <form onSubmit={this.addPerson}>
+          <div>
+            nimi: <input
+              value={this.state.newName}
+              onChange={this.handleNameChange} />
+          </div>
+          <div>
+            <button type="submit">lisää</button>
+          </div>
+        </form>
+        <h2>Numerot</h2>
+        <Persons persons={this.state.persons} />
+      </div>
+    )
+  }
 }
 
 export default App
